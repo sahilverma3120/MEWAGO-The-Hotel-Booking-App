ad20b22045df5636f557ff234ea9a2053cf0d6b9
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-09-14 00:06:22 +0530

    Add files via upload

--- client/src/auth/Login.js
+++ client/src/auth/Login.js
@@ -2,10 +2,13 @@ import { useState } from "react";
 import { toast } from "react-toastify";
 import { login } from "../actions/auth";
 import LoginForm from "../components/LoginForm";
+import { useDispatch } from "react-redux";
 
-const Login = () => {
+const Login = ({ history }) => {
   const [email, setEmail] = useState("ryan@gmail.com");
   const [password, setPassword] = useState("rrrrrr");
+
+  const dispatch = useDispatch();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -17,7 +20,15 @@ const Login = () => {
         console.log(
           "SAVE USER RES IN REDUX AND LOCAL STORAGE THEN REDIRECT ===> "
         );
-        console.log(res.data);
+        // console.log(res.data);
+        // save user and token to local storage
+        window.localStorage.setItem("auth", JSON.stringify(res.data));
+        // save user and token to redux
+        dispatch({
+          type: "LOGGED_IN_USER",
+          payload: res.data,
+        });
+        history.push("/dashboard");
       }
     } catch (err) {
       console.log(err);
